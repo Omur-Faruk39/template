61e8ba4164f692656e226e8cfef9276c32547428
Omur-Faruk39 <omurfaruk39bd@gmail.com>
2025-06-21 09:35:16 +0600

    pacticing shouldcompnent update life cycle method

--- router/react-router/src/App.jsx
+++ router/react-router/src/App.jsx
@@ -1,14 +1,36 @@
-import { createBrowserRouter, Route, Routes } from "react-router-dom";
+import { PureComponent } from "react";
 import Home from "./components/Home";
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route element={<Home />} path="/" />
-      </Routes>
-    </>
-  );
+class App extends PureComponent {
+  state = {
+    name: "sakib",
+    age: 23,
+    counter: 0,
+  };
+
+  handler = () => {
+    // console.log("hel");
+    this.setState((pre) => {
+      return { ...pre, name: "tamim", age: 32 };
+    });
+  };
+  render() {
+    return (
+      <>
+        <Home info={this.state} />
+        <button onClick={this.handler}>click me</button>
+        <button
+          onClick={() => {
+            this.setState((pre) => {
+              return { ...pre, counter: pre.counter + 1 };
+            });
+          }}
+        >
+          counter {this.state.counter}
+        </button>
+      </>
+    );
+  }
 }
 
 export default App;
--- router/react-router/src/components/Home.jsx
+++ router/react-router/src/components/Home.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-function Home() {
-  return <>this is home page.</>;
+class Home extends React.PureComponent {
+  shouldComponentUpdate(nextPros) {
+    if (
+      (this.props.info.name === nextPros.info.name) &
+      (this.props.info.age === nextPros.info.age)
+    ) {
+      return false;
+    }
+    return true;
+  }
+  render() {
+    console.log("render");
+    return (
+      <>
+        <h1>the name is {this.props.info.name}</h1>
+        <h1>the age is {this.props.info.age}</h1>
+      </>
+    );
+  }
 }
 
 export default Home;
